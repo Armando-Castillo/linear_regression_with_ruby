d82288c31ebb3b3275424cd9fa75705a696d157f
Armando-Castillo <armandocreyes02@gmail.com>
2021-10-03 19:03:08 -0500

    comments for bias, slope and prediction

--- app/controllers/api/ala/predictions_controller.rb
+++ app/controllers/api/ala/predictions_controller.rb
@@ -9,14 +9,15 @@ class Api::Ala::PredictionsController < ApplicationController
 
         x_data = Array.new(y_data.length) { |i| i = i+1 }
         
-        #Get slope (m/b1/a)
-        slope = get_slope(y_data, x_data)
-        bias = get_bias(y_data, x_data, slope)
+        slope = get_slope(y_data, x_data) #Returns slope (pendiente/m)
+        bias = get_bias(y_data, x_data, slope) #Return bias (tendencia/b/y_intersection)
 
         prediction_dataset = []
-        prediction_dataset = get_prediction_dataset(slope, bias, x_data)
+        #Returns linear regression for all x_data and 1 more (# more custom in funcition)
+        prediction_dataset = get_prediction_dataset(slope, bias, x_data) 
 
         prediction_next_month = 0
+        #Returns next prediction
         prediction_next_month = prediction_dataset.last
 
         render json: {
@@ -28,42 +29,42 @@ class Api::Ala::PredictionsController < ApplicationController
     end
 
     def get_slope(y_data, x_data)
-        slope = 0
+        slope = 0 #data_error_sum/x_stand_errors_sum
 
         y_data_mean = 0
-        y_data_mean = y_data.inject{ |sum, i| sum + i }.to_f / y_data.size
+        y_data_mean = y_data.inject{ |sum, i| sum + i }.to_f / y_data.size #Mean for y_data
 
         x_data_mean = 0
-        x_data_mean = x_data.inject{ |sum, i| sum + i }.to_f / x_data.size
+        x_data_mean = x_data.inject{ |sum, i| sum + i }.to_f / x_data.size #Mean for x_data
 
         y_error = []
         for value in y_data do
-            y_error.push << value - y_data_mean
+            y_error.push << value - y_data_mean #errors array for y_data
         end
 
         x_error = []
         for value in x_data do
-            x_error.push << value - x_data_mean
+            x_error.push << value - x_data_mean #errors array for x_data
         end
 
         data_single_errors = []
         y_error.zip(x_error).each do |y_value, x_value|
-            data_single_errors << y_value*x_value
+            data_single_errors << y_value*x_value #errors array for data
         end
         
         data_error_sum = 0
         for value in data_single_errors do
-            data_error_sum = data_error_sum + value
+            data_error_sum = data_error_sum + value #total sum of errors array for data
         end
 
         x_standardized_errors = []
         for value in x_error do
-            x_standardized_errors.push << value**2
+            x_standardized_errors.push << value**2 #array for x_standardized_errors
         end
 
         x_stand_errors_sum = 0
         for value in x_standardized_errors do
-            x_stand_errors_sum = x_stand_errors_sum + value
+            x_stand_errors_sum = x_stand_errors_sum + value #sum of x_standardized_errors
         end
 
         slope = data_error_sum/x_stand_errors_sum
@@ -86,7 +87,7 @@ class Api::Ala::PredictionsController < ApplicationController
     end
 
     def get_prediction_dataset(slope, bias, x_data)
-        slope_values = []
+        slope_values = [] #y=mx+b // slope_values = slope*x_data[i]+bias
         x_data << (x_data.last + 1)
         for value in x_data do
             slope_values << (slope*value) + bias
